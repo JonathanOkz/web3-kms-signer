d6d4910788c45f6338f665704bb6d303742dd0ce
Jonathan Okz <jonathanolesz@gmail.com>
2024-02-16 00:29:59 +0100

    update hd-wallets

--- packages/hd-wallets/src/HDWallets.ts
+++ packages/hd-wallets/src/HDWallets.ts
@@ -1,4 +1,6 @@
-import { ECDSASignature, Wallets, HDProvider } from "@web3-kms-signer/core";
+import * as ethutil from "@ethereumjs/util";
+import { ethers } from "ethers";
+import { UPublickey, Wallets, ECDSASignature } from "@web3-kms-signer/core";
 
 /**
  * The HDWallets class extends the abstract Wallets class, providing a concrete implementation
@@ -10,11 +12,11 @@ import { ECDSASignature, Wallets, HDProvider } from "@web3-kms-signer/core";
  *
  */
 export class HDWallets extends Wallets {
-    private provider: HDProvider
+    private nodeWallet: ethers.HDNodeWallet;
 
-    constructor(provider: HDProvider) {
+    constructor(phrase: string) {
         super();
-        this.provider = provider;
+        this.nodeWallet = ethers.HDNodeWallet.fromPhrase(phrase);
     }
 
     /**
@@ -22,26 +24,49 @@ export class HDWallets extends Wallets {
      * @returns The name of the provider as a string.
      */
     get PROVIDER(): string {
-        return `HD_WALLETS_${this.provider.constructor.name}`
+        return `HD_WALLETS`
     }
 
     /**
-     * Retrieves the public key for a given key identifier, using the associated HDProvider.
-     * @param keyId The identifier for the key.
-     * @returns Promise resolving to the public key as a Buffer.
+     * Retrieves the public key corresponding to a given key identifier (keyId) as a Buffer.
+     *
+     * This method first derives the private key associated with the specified keyId by calling
+     * the `getPrivateKey` method.
+     *
+     * @param {string} keyId - The BIP44 key derivation path used to derive the private key.
+     * @returns {Buffer} The derived public key as a Buffer, ready for cryptographic uses.
+     * @throws {Error} Throws an error if the private key cannot be derived.
      */
-    public async getPublickey(keyId: string) : Promise<Buffer> {
-        return this.provider.getPublickey(keyId);
+    public async getPublickey(keyId: string) : Promise<Buffer>  {
+        return UPublickey.fromPrivatekey(this.getPrivatekey(keyId)).getPublickey();
     }
 
     /**
-     * Signs a digest with the specified key identifier and optional chain ID, using the associated HDProvider.
-     * @param keyId The identifier for the key.
-     * @param digest The message digest to sign.
-     * @param chainId Optional chain ID to specify the blockchain network.
-     * @returns Promise resolving to an ECDSASignature.
+     * Signs a given digest using the ECDSA (Elliptic Curve Digital Signature Algorithm) with a private
+     * key derived from a specified key identifier (keyId).
+     *
+     * @param {string} keyId - The BIP44 key derivation path used to derive the private key for signing.
+     * @param {Buffer} digest - The message digest to sign. This should be a 32-byte hash.
+     * @param {bigint} [chainId] - Optional chain ID to specify the blockchain network.
+     * @returns {ECDSASignature} An object containing the components of the ECDSA signature (`r`, `s`, `v`).
+     * @throws {Error} Throws an error if the signing process fails or if the private key cannot be derived.
      */
     public async ecsign(account: { keyId: string }, digest: Buffer, chainId?: bigint) : Promise<ECDSASignature> {
-        return this.provider.ecsign(account.keyId, digest, chainId);
+        return ethutil.ecsign(digest, this.getPrivatekey(account.keyId), chainId);
+    }
+
+    /**
+     * Retrieves the private key associated with a given key identifier (keyId) as a Buffer.
+     *
+     * This method derives the private key from a node wallet using the provided keyId, which
+     * represents a BIP44 key derivation path.
+     *
+     * @param {string} keyId - The BIP44 key derivation path used to derive the private key.
+     * @returns {Buffer} The derived private key as a Buffer, suitable for cryptographic operations.
+     * @throws {Error} Throws an error if the private key cannot be derived or processed correctly.
+     */
+    private getPrivatekey(keyId: string) : Buffer {
+        const privateKey = this.nodeWallet.derivePath(keyId).privateKey
+        return Buffer.from(privateKey.substring(2,66), "hex");
     }
 }
